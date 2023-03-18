261f68f116c7f45aa5a5e8f7c57bbb03ac643b56
lucas-rodriguez <lrodriguez.arevalosc@gmail.com>
2023-03-18 20:45:59 -0300

    add typeDefs and resolvers

--- server/app.js
+++ server/app.js
@@ -1,18 +1,22 @@
 import express from 'express'
 import { ApolloServer } from '@apollo/server'
-import { expressMiddleware } from '@apollo/server'
+import { expressMiddleware } from '@apollo/server/express4'
 import cors from 'cors'
+import http from 'http'
 
 
-export async function startApolloServer() {
+const PORT = process.env.PORT
+
+export async function startApolloServer(typeDefs, resolvers) {
     const app = express()
+    const httpServer = http.createServer()
 
 const server = new ApolloServer({
-    typeDefs: '',
-    resolvers: ()=>{
-
-    }
+    typeDefs: typeDefs,
+    resolvers: resolvers
 })
 await server.start();
 app.use('/graphql', cors(), express.json(), expressMiddleware(server))
+
+await new Promise(resolve => httpServer.listen({port : PORT || 4000}))
 }
--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 import { startApolloServer } from './app.js'
 import dotenv from 'dotenv'
+import { typeDefs } from './graphql/typeDefs.js'
+import { resolvers } from './graphql/resolvers.js'
 
 
 dotenv.config();
 
+startApolloServer(typeDefs, resolvers)
 
-const PORT = process.env.PORT
-app.listen(PORT || 3001)
-console.log(`Server listening on port : ${PORT}`);
