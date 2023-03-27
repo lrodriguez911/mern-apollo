8abcb0dfe5fd4291d59e12087e001d4e84330fb7
lucas-rodriguez <lrodriguez.arevalosc@gmail.com>
2023-03-26 22:47:12 -0300

    complete configurarion to make a mutation using graphql

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -1,5 +1,15 @@
+import Project from "../models/Project.js";
+
 export const resolvers = {
     Query: {
         hello: () => "Hello World!"
+    },
+    Mutation: {
+        createProject: async (_, {name, description}) =>{
+            const project = new Project({name, description})
+            const saveproject = await project.save()
+            console.log(name, description);
+            return saveproject;
+        }
     }
 }
