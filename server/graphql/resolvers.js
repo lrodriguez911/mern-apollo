0aa86a1cf3f9a06fa8ec3148a00b36dd57d50f8f
lucas-rodriguez <lrodriguez.arevalosc@gmail.com>
2023-03-27 20:59:10 -0300

    add tasks list resolver and type definition

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -1,11 +1,20 @@
 import Project from "../models/Project.js";
+import Task from "../models/Task.js";
 
 export const resolvers = {
     Query: {
-        hello: () => "Hello World!"
+        hello: () => "Hello World!",
+        projects: async () => await Project.find(),
+        tasks: async () => await Task.find()
     },
     Mutation: {
         createProject: async (_, {name, description}) =>{
+            const project = new Project({name, description})
+            const saveproject = await project.save()
+            console.log(name, description);
+            return saveproject;
+        }
+        createTask: async (_, {title, description}) =>{
             const project = new Project({name, description})
             const saveproject = await project.save()
             console.log(name, description);
